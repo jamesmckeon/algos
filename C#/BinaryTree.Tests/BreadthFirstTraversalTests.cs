d07ae4f9c0c54a103e476440de1d2c6ee9791986
jamesmckeon <jbmckeon@comcast.net>
2018-02-13 10:11:58 -0800

    finished BFT

--- C#/BinaryTree.Tests/BreadthFirstTraversalTests.cs
+++ C#/BinaryTree.Tests/BreadthFirstTraversalTests.cs
@@ -14,92 +14,12 @@ namespace Tests.BinaryTree
         BreadthFirstTraversal Target { get; set; } = new BreadthFirstTraversal();
 
         [Fact]
-        public void TraverseRecursive_NullRoot_ReturnsNull()
+        public void TraverseRecursive_NotImplemented()
         {
-            Assert.Null(Target.TraverseRecursive(null));
-        }
+            //recursive BFT is a waste of time
+            var ex = Assert.Throws<NotImplementedException>(() => Target.TraverseRecursive(null));
+            Assert.NotNull(ex);
 
-        [Fact]
-        public void TraverseRecursive_Case1()
-        {
-            var expected = new int[] { 1 };
-            var root = new TreeNode(1);
-            Assert.Equal(expected, Target.TraverseRecursive(root));
-        }
-
-        [Fact]
-        public void TraverseRecursive_Case2()
-        {
-            var expected = new int[] { 1, 2 };
-            var root = new TreeNode(1);
-            root.left = new TreeNode(2);
-            Assert.Equal(expected, Target.TraverseRecursive(root));
-        }
-
-        [Fact]
-        public void TraverseRecursive_Case3()
-        {
-            var expected = new int[] { 1, 2, -1 };
-            var root = new TreeNode(1);
-            root.left = new TreeNode(2);
-            root.right = new TreeNode(-1);
-            Assert.Equal(expected, Target.TraverseRecursive(root));
-        }
-
-        [Fact]
-        public void TraverseRecursive_Case4()
-        {
-            var expected = new int[] { 1, 2, -1, 0 };
-            var root = new TreeNode(1);
-            root.left = new TreeNode(2);
-            root.right = new TreeNode(-1);
-            root.left.left = new TreeNode(0);
-
-            Assert.Equal(expected, Target.TraverseRecursive(root));
-        }
-
-        [Fact]
-        public void TraverseRecursive_Case5()
-        {
-            var expected = new int[] { 1, 2, -1, 0, 4 };
-            var root = new TreeNode(1);
-            root.left = new TreeNode(2);
-            root.right = new TreeNode(-1);
-            root.left.left = new TreeNode(0);
-            root.left.right = new TreeNode(4);
-
-            Assert.Equal(expected, Target.TraverseRecursive(root));
-        }
-
-        [Fact]
-        public void TraverseRecursive_Case6()
-        {
-            var expected = new int[] { 1, 2, -1, 0, 4, -5 };
-            var root = new TreeNode(1);
-            root.left = new TreeNode(2);
-            root.right = new TreeNode(-1);
-            root.left.left = new TreeNode(0);
-            root.left.right = new TreeNode(4);
-            root.right.left = new TreeNode(-5);
-
-            Assert.Equal(expected, Target.TraverseRecursive(root));
-        }
-
-        [Fact]
-        public void TraverseRecursive_Case7()
-        {
-            var expected = new int[] { 1, 2, -1, 0, 4, -5, 8 };
-            var root = new TreeNode(1);
-            root.left = new TreeNode(2);
-            root.right = new TreeNode(-1);
-            root.left.left = new TreeNode(0);
-            root.left.right = new TreeNode(4);
-            root.right.left = new TreeNode(-5);
-            root.right.right = null;
-            root.left.left.left = null;
-            root.left.left.right = new TreeNode(8);
-
-            Assert.Equal(expected, Target.TraverseRecursive(root));
         }
 
         [Fact]
--- C#/BinaryTree/BreadthFirstTraversal.cs
+++ C#/BinaryTree/BreadthFirstTraversal.cs
@@ -10,47 +10,9 @@ namespace Algos.BinaryTree
     {
         public int[] TraverseRecursive(TreeNode node)
         {
-            if (node == null)
-            {
-                return null;
-            }
-            else
-            {
-                var values = new List<int>();
-                values.Add(node.val);
-           return     TraverseRecursive(node, values);
-
-                
-            }
+            throw new NotImplementedException();
         }
 
-        public int[] TraverseRecursive(TreeNode node, IList<int> values)
-        {
-
-                if (node.left != null )
-                {
-                values.Add(node.left.val);
-                }
-
-                if ( node.right != null)
-                {
-                values.Add(node.right.val);
-                  }
-
-            if (node.left != null)
-            {
-
-                TraverseRecursive(node.left, values);
-            }
-
-            if (node.right != null)
-            {
- 
-                TraverseRecursive(node.right, values);
-            }
-
-            return values.ToArray();
-        }
 
         public int[] TraverseIterative(TreeNode node)
         {
@@ -75,7 +37,7 @@ namespace Algos.BinaryTree
                         q.Enqueue(node.left);
                     }
 
-                   if(node.right != null)
+                    if (node.right != null)
                     {
                         q.Enqueue(node.right);
                     }
