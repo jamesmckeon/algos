df56687e72eb82d03e83439ade9fb26513cb27ee
jamesmckeon <jbmckeon@comcast.net>
2018-02-13 10:22:51 -0800

    added BST project

---
+++ C#/BinarySearchTree/BST.cs
@@ -0,0 +1,25 @@
+using System;
+using Algos.BinaryTree;
+
+public class BST
+{
+
+protected TreeNode _root = null;
+
+    public void Insert(int val)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(int val)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TreeNode Root{
+        get
+        {
+return _root;
+        }
+    }
+}
